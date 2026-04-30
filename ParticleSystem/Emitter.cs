08c4c5e9fe7845f50c5b5b9372875d3875864764
Ksenialalala <paksinia@internet.ru>
2026-05-01 00:46:08 +0800

    добавила равномерную генерацию частиц

--- ParticleSystem/Emitter.cs
+++ ParticleSystem/Emitter.cs
@@ -15,14 +15,37 @@ namespace ParticleSystem
         public float GravitationX = 0;
         public float GravitationY = 0;
 
+        public int ParticlesCount = 500;
+
+        public int X; 
+        public int Y; 
+        public int Direction = 0; 
+        public int Spreading = 360; 
+        public int SpeedMin = 1; 
+        public int SpeedMax = 10; 
+        public int RadiusMin = 2; 
+        public int RadiusMax = 10; 
+        public int LifeMin = 20; 
+        public int LifeMax = 100;
+        public int ParticlesPerTick = 1;
+
+        public Color ColorFrom = Color.White; 
+        public Color ColorTo = Color.FromArgb(0, Color.Black);
+
         public void UpdateState()
         {
+            int particlesToCreate = ParticlesPerTick;
+
             foreach (var particle in particles)
             {
-                particle.Life -= 1;
-                if (particle.Life < 0)
+                if (particle.Life <= 0)
                 {
-                    ResetParticle(particle);
+                    if (particlesToCreate > 0)
+                    {
+                        
+                        particlesToCreate -= 1; 
+                        ResetParticle(particle);
+                    }
                 }
                 else
                 {
@@ -39,22 +62,12 @@ namespace ParticleSystem
                 }
             }
 
-            for (var i = 0; i < 10; ++i)
+            while (particlesToCreate >= 1)
             {
-                if (particles.Count < 500)
-                {
-                    var particle = new ParticleColorful();
-                    particle.FromColor = Color.White;
-                    particle.ToColor = Color.FromArgb(0, Color.Black);
-
-                    ResetParticle(particle); 
-
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break;
-                }
+                particlesToCreate -= 1;
+                var particle = CreateParticle();
+                ResetParticle(particle);
+                particles.Add(particle);
             }
         }
 
@@ -122,17 +135,21 @@ namespace ParticleSystem
 
         public virtual void ResetParticle(Particle particle)
         {
-            particle.Life = 20 + Particle.rand.Next(100);
-            particle.X = MousePositionX;
-            particle.Y = MousePositionY;
+            particle.Life = Particle.rand.Next(LifeMin, LifeMax);
 
-            var direction = (double)Particle.rand.Next(360);
-            var speed = 1 + Particle.rand.Next(10);
+            particle.X = X;
+            particle.Y = Y;
+
+            var direction = Direction
+                + (double)Particle.rand.Next(Spreading)
+                - Spreading / 2;
+
+            var speed = Particle.rand.Next(SpeedMin, SpeedMax);
 
             particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
             particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
 
-            particle.Radius = 2 + Particle.rand.Next(10);
+            particle.Radius = Particle.rand.Next(RadiusMin, RadiusMax);
         }
 
         public class TopEmitter : Emitter
@@ -149,6 +166,15 @@ namespace ParticleSystem
                 particle.SpeedY = 1; 
                 particle.SpeedX = Particle.rand.Next(-2, 2);  
             }
+        }
+
+        public virtual Particle CreateParticle()
+        {
+            var particle = new ParticleColorful();
+            particle.FromColor = ColorFrom;
+            particle.ToColor = ColorTo;
+
+            return particle;
         }
     }
 }
